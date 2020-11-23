025dfb6f119c4a926c6bd4ddc46fca9b337ce4eb
matheus3006 <matheussouza18@live.com>
2020-11-23 17:47:24 -0300

    get user profile add(/profile/me - route)

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,38 @@
 const express  = require('express');
 const router = express.Router();
 
+const auth = require('../../middleware/auth');
 
-//@route GET api/profile
-//@desc Test route
-//@access    Public
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+
+
+//@route    GET api/profile
+//@desc     Test route
+//@access   Public
 router.get('/', (req,res)=> res.send('Profile route'));
+
+// @router  GET api/profile/me
+// @desc    Get current users profile
+// @access  Private
+router.get('/me',auth,async (req,res)=> {
+    try {
+        const profile = await (Profile.findOne({user: req.user.id})).populate('user',['name','avatar']);
+
+        if(!profile){
+            return res.status(400).json({msg: 'there is no profile for this user'});
+        }
+        res.json(profile);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('server error');
+    }
+
+
+
+});
+
+
 
 module.exports = router;
